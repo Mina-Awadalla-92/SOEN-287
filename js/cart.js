319e703364f161326c5841665bcb1d108a3146db
Reuven1203 <reuven1203@gmail.com>
2021-10-30 02:05:47 -0400

    .

--- js/cart.js
+++ js/cart.js
@@ -1,33 +1,92 @@
+class item{
+  constructor(name,price,unit,imgSrc) {
+    this.name = name;
+    this.price = price;
+    this.unit = unit;
+    this.imgSrc = imgSrc;
+  }
+}
+
+let itemName;
+let priceWunit;
+let imageSrc;
 var addToCartButton = document.getElementsByClassName("btn btn-dark");
 for (var i = 0; i < addToCartButton.length; i++) {
     addToCartButton[i].addEventListener('click', addToCartClicked);
 
 }
 
-
-function addToCartClicked(a) {
-  var divParent = a.target.parentElement;
-  var itemName = divParent.querySelector(".card-title").textContent;
-  var price = divParent.querySelector(".cost").textContent;
-  var priceDouble = parseFloat(price);
-  var priceWunit = '$' + price + divParent.querySelector(".unit").textContent
-  var imageSrc = divParent.parentElement.querySelector(".item-pic").src;
-  console.log(imageSrc);
-  addToCart(itemName,priceWunit,imageSrc);
+function loadItems() {
+  var products = document.getElementsByClassName("card");
   
 }
 
-function addToCart(itemName,priceWunit,imageSrc) {
-  var cartRow = document.createElement('tr');
-  var cartCell = document.createElement('td');
-  var itemImage = document.createElement('img');
-  var itemTitle = itemName;
+function addToCartClicked(a) {
+  var divParent = a.target.parentElement;
+  itemName = divParent.querySelector(".card-title").textContent;
+  console.log(itemName)
+  var price = divParent.querySelector(".cost").textContent;
+  var priceDouble = parseFloat(price);
+  priceWunit = '$' + priceDouble + divParent.querySelector(".unit").textContent
+  imageSrc = divParent.parentElement.querySelector(".item-pic").src;
   
+  
+}
+
+function addToCart() {
+  console.log(itemName);
+  var cartRow = document.createElement('tr');
+  //First cell
+  var cartCell1 = document.createElement('td');
+  var itemImage = document.createElement('img');
   itemImage.src = imageSrc;
+  var title = document.createElement('p');
+  title.innerHTML = itemName;
+  cartCell1.appendChild(itemImage);
+  cartCell1.appendChild(title);
+
+  //second cell
+  var cartCell2 = document.createElement('td');
+  var pricePerunit = document.createElement('h6');
+  pricePerunit.textContent = priceWunit;
+  cartCell2.appendChild(pricePerunit);
+
+  //third cell
+  var cartCell3 = document.createElement('td');
+  var amount = document.createElement('input');
+  amount.type = "number";
+  amount.id = itemName;
+  amount.className = "shoppinginput";
+  amount.onchange = "grandtotalfruits";
+  amount.value = "1"
+  amount.min = "0"
+  cartCell3.appendChild(amount);
+
+  //fourthCell
+  var cartCell4 = document.createElement('td');
+  var dollarSign = document.createElement('span');
+  dollarSign.innerHTML = "$";
+  var outputPrice = document.createElement('output')
+  var button = document.createElement('button');
+  button.type = "button";
+  button.className = "btn btn-dark btn1 remove";
+  button.textContent = "Remove";
+  cartCell4.appendChild(dollarSign);
+  cartCell4.appendChild(outputPrice);
+  cartCell4.appendChild(button);
+
+
+
+  cartRow.appendChild(cartCell1);
+  cartRow.appendChild(cartCell2);
+  cartRow.appendChild(cartCell3);
+  cartRow.appendChild(cartCell4);
+  
+  
   var cartItems = document.querySelector('tbody');
   cartItems.appendChild(cartRow);
-  cartRow.appendChild(cartCell);
-  cartCell.appendChild(itemImage)
+  
+  
 
 }
 
