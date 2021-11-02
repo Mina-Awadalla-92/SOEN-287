53b893518f8bda76907420cbc2f0230e13149016
Reuven1203 <reuven1203@gmail.com>
2021-11-02 15:44:42 -0400

    .

--- js/cart2.js
+++
@@ -1,7 +0,0 @@
-// document.getElementById("kcost").onchange = kiwiCost;
-document.getElementById("kcost").onclick = kiwiCost;
-document.getElementById("subCost").onchange = Subtotal;
-document.getElementById("afterTax").onchange = afterTax;
-
-
-
