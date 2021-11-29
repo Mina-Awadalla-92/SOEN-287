b77dae84f219cff5d98345d542ba78344e3492f8
Reuven1203 <reuven1203@gmail.com>
2021-11-28 22:28:44 -0500

    Update signuppage.php

--- Aisle/signuppage.php
+++ Aisle/signuppage.php
@@ -24,7 +24,7 @@
  session_start();
  
       
-      if(isset($_REQUEST['signUp'])){
+      if(isset($_POST['signUp'])){
         echo "hello world";
         $xml = new DOMDocument("1.0","UTF-8");
         $xml -> load("../Database/user.xml");
@@ -32,14 +32,14 @@
         $rootTag = $xml -> getElementsByTagName("Users")->item(0);
         $userTag = $xml -> createElement('user');
 
-        $nameTag = $xml -> createElement('firstName',  $_REQUEST['firstName']);
-        $lNameTag = $xml -> createElement('lastName', $_REQUEST['lastName']);
-        $addresTag = $xml -> createElement('address', $_REQUEST['address']);
-        $cityTag = $xml -> createElement('city', $_REQUEST['city']);
-        $provTag = $xml -> createElement('province', $_REQUEST['province']);
-        $postalTag = $xml -> createElement('postalCode', $_REQUEST['postalCode']);
-        $emailTag = $xml -> createElement('email', $_REQUEST['email']);
-        $passwrodTag = $xml -> createElement('password', $_REQUEST['password']);
+        $nameTag = $xml -> createElement('firstName',  $_POST['firstName']);
+        $lNameTag = $xml -> createElement('lastName', $_POST['lastName']);
+        $addresTag = $xml -> createElement('address', $_POST['address']);
+        $cityTag = $xml -> createElement('city', $_POST['city']);
+        $provTag = $xml -> createElement('province', $_POST['province']);
+        $postalTag = $xml -> createElement('postalCode', $_POST['postalCode']);
+        $emailTag = $xml -> createElement('email', $_POST['email']);
+        $passwrodTag = $xml -> createElement('password', $_POST['password']);
         
         $userTag -> appendChild($nameTag);
         $userTag -> appendChild($lNameTag);
