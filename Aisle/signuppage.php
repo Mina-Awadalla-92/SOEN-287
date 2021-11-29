a109565c80e97f33110307bd2cd117d91ddad4fb
Reuven1203 <reuven1203@gmail.com>
2021-11-28 23:51:43 -0500

    Update signuppage.php

--- Aisle/signuppage.php
+++ Aisle/signuppage.php
@@ -1,5 +1,40 @@
 
+<?php
+      session_start();
+ 
+      
+      if(isset($_REQUEST['signUp'])){
+        
+        $xml = new DOMDocument("1.0","UTF-8");
+        $xml -> load("Database/user.xml");
 
+        $rootTag = $xml -> getElementsByTagName("Users")->item(0);
+        $userTag = $xml -> createElement('user');
+
+        $nameTag = $xml -> createElement('firstName',  $_POST['firstName']);
+        $lNameTag = $xml -> createElement('lastName', $_POST['lastName']);
+        $addresTag = $xml -> createElement('address', $_POST['address']);
+        $cityTag = $xml -> createElement('city', $_POST['city']);
+        $provTag = $xml -> createElement('province', $_POST['province']);
+        $postalTag = $xml -> createElement('postalCode', $_POST['postalCode']);
+        $emailTag = $xml -> createElement('email', $_POST['email']);
+        $passwrodTag = $xml -> createElement('password', $_POST['password']);
+        
+        $userTag -> appendChild($nameTag);
+        $userTag -> appendChild($lNameTag);
+        $userTag -> appendChild($addresTag);
+        $userTag -> appendChild($cityTag);
+        $userTag -> appendChild($provTag);
+        $userTag -> appendChild($postalTag);
+        $userTag -> appendChild($emailTag);
+        $userTag -> appendChild($passwrodTag);
+        
+        $rootTag -> appendChild($userTag);
+
+        echo ($_POST['firstName']);
+        echo $xml->save("Database/user.xml");
+      }
+      ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -89,42 +124,7 @@
       </div>
     </nav>
   </div>
-  <?php
-      session_start();
- 
-      
-      if(isset($_REQUEST['signUp'])){
-        
-        $xml = new DOMDocument("1.0","UTF-8");
-        $xml -> load("Database/user.xml");
-
-        $rootTag = $xml -> getElementsByTagName("Users")->item(0);
-        $userTag = $xml -> createElement('user');
-
-        $nameTag = $xml -> createElement('firstName',  $_POST['firstName']);
-        $lNameTag = $xml -> createElement('lastName', $_POST['lastName']);
-        $addresTag = $xml -> createElement('address', $_POST['address']);
-        $cityTag = $xml -> createElement('city', $_POST['city']);
-        $provTag = $xml -> createElement('province', $_POST['province']);
-        $postalTag = $xml -> createElement('postalCode', $_POST['postalCode']);
-        $emailTag = $xml -> createElement('email', $_POST['email']);
-        $passwrodTag = $xml -> createElement('password', $_POST['password']);
-        
-        $userTag -> appendChild($nameTag);
-        $userTag -> appendChild($lNameTag);
-        $userTag -> appendChild($addresTag);
-        $userTag -> appendChild($cityTag);
-        $userTag -> appendChild($provTag);
-        $userTag -> appendChild($postalTag);
-        $userTag -> appendChild($emailTag);
-        $userTag -> appendChild($passwrodTag);
-        
-        $rootTag -> appendChild($userTag);
-
-        echo ($_POST['firstName']);
-        echo $xml->save("Database/user.xml");
-      }
-      ?>
+  
   <div class="login">
     <div class= "login-text" >Sign Up</div >
       <br><br><br>
