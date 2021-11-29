08b83cc194f36431f83cd33dbd4d3a7a786397de
Reuven1203 <reuven1203@gmail.com>
2021-11-28 22:55:01 -0500

    Update signuppage.php

--- Aisle/signuppage.php
+++ Aisle/signuppage.php
@@ -133,7 +133,7 @@
       
       
      <div class="textboxSignup">
-      <form action="signuppage.php" , method = "POST"> 
+      <form action="" , method = "POST"> 
         <input type="firstName" placeholder = "First Name" name="firstName"><br>
         <input type="lastName" placeholder = "Last Name" action="" name="lastName"><br>
         <input type="address" placeholder = "Street Address" action="" name="address"><br>
