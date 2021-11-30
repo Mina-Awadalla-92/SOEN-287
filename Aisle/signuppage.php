b1d0d60bf3b4844bd0af860f3ae645a73e97c503
Reuven1203 <reuven1203@gmail.com>
2021-11-30 00:57:29 -0500

    ..

--- Aisle/signuppage.php
+++ Aisle/signuppage.php
@@ -138,36 +138,7 @@
 </footer>
 </html>
 <?php
- if(isset($_POST['signUp'])){
-        
-  $xml = new DOMDocument("1.0","UTF-8");
-  $xml -> load("../Database/user.xml");
-
-  $rootTag = $xml -> getElementsByTagName("Users")->item(0);
-  $userTag = $xml -> createElement('user');
-
-  $nameTag = $xml -> createElement('firstName',  $_POST['firstName']);
-  $lNameTag = $xml -> createElement('lastName', $_POST['lastName']);
-  $addresTag = $xml -> createElement('address', $_POST['address']);
-  $cityTag = $xml -> createElement('city', $_POST['city']);
-  $provTag = $xml -> createElement('province', $_POST['province']);
-  $postalTag = $xml -> createElement('postalCode', $_POST['postalCode']);
-  $emailTag = $xml -> createElement('email', $_POST['email']);
-  $passwrodTag = $xml -> createElement('password', $_POST['password']);
-  
-  $userTag -> appendChild($nameTag);
-  $userTag -> appendChild($lNameTag);
-  $userTag -> appendChild($addresTag);
-  $userTag -> appendChild($cityTag);
-  $userTag -> appendChild($provTag);
-  $userTag -> appendChild($postalTag);
-  $userTag -> appendChild($emailTag);
-  $userTag -> appendChild($passwrodTag);
-  
-  $rootTag -> appendChild($userTag);
-
-  $xml->save("../Database/user.xml"); 
- }
+ include("backstore.php");
 ?>
 
 
